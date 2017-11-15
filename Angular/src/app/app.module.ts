2ab595bd35b88997712f5d2d635847c70e83c269
Dario <loller790@gmail.com>
2017-11-15 16:07:53 +0100

    Angular: Add Login, Menu, Events List & New Event Components

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
+import { EventsListComponent } from './events-list/events-list.component';
+import { NewEventComponent } from './new-event/new-event.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MenuComponent,
+    EventsListComponent,
+    NewEventComponent
   ],
   imports: [
     BrowserModule
