3ff7ffca3995e363328172305a4fd21d88fe7181
Dario <loller790@gmail.com>
2017-11-16 11:05:36 +0100

    Angular: Introduce sessionStatus
    Session Statuses:
    
    0 => Not logged in
    1 => Logged in
    2 => Logged in + New event

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  // Session Status: 0 => Not logged in // 1 => Logged in // 2 => Logged in + New event
+  sessionStatus:string = "0";
+
+  constructor() {
+    let currentSession = sessionStorage.getItem("sessionStatus");
+    if (currentSession !== null) {
+      this.sessionStatus = currentSession;
+    }
+    console.log(this.sessionStatus);
+  }
 }
