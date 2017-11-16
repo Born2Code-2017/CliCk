9a29bcb13febff0e465b5bf0bb275971d07d21a3
mb180594b <mb180594b@gmail.com>
2017-11-16 12:57:46 +0100

    Angular->Menu: Add Profile Info & User Component

--- Angular/src/app/menu/menu.component.ts
+++ Angular/src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './menu.component.module';
 
 @Component({
   selector: 'app-menu',
@@ -6,6 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
+  public userDB: User[];
 
   public logOut() {
     sessionStorage.setItem("sessionStatus", "0");
@@ -13,7 +15,53 @@ export class MenuComponent {
   }
 
   constructor() {
-
+    this.userDB = [
+      {
+        id: 0,
+        email: "ilaria.kazuko@email.com",
+        password: "japan",
+        location: "Florence",
+        age: 27,
+        job: "Fashion Blogger - Fitness Teacher",
+        name: "Ilaria Kazuko"
+      },
+      {
+        id: 1,
+        email: "fabio.russo@email.com",
+        password: "russia",
+        location: "Milan",
+        age: 40,
+        job: "Model Agency Owner",
+        name: "Fabio Russo"
+      },
+      {
+        id: 2,
+        email: "luca.fabiani@email.com",
+        password: "italy",
+        location: "Venice",
+        age: 45,
+        job: "Fashion Designer",
+        name: "Luca Fabiani"
+      },
+      {
+        id: 3,
+        email: "stefania.muller@email.com",
+        password: "germany",
+        location: "Rome",
+        age: 37,
+        job: "Photographer",
+        name: "Stefania Muller"
+      },
+      {
+        id: 4,
+        email: "fernando.giannitrapani@email.com",
+        password: "america",
+        location: "Milan",
+        age: 32,
+        job: "Hairstylist - Make-Up Artist",
+        name: "Fernando Giannitrapani"
+      }
+    ]
   }
 
 }
