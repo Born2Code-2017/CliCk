469bae3219fa4f16845dac9f13181b9a7230349b
ado-ado <adolfinia@gmail.com>
2017-11-19 18:16:05 +0100

    Angular->App:Module ->Login: Ts, Css, Html
    ->App: Imported ReactiveFormsModule
    ->Login:
    Html: Better use of Bootstrap and addition of conditionals
    Css: Red border for login failure
    Ts: Changed text of login failure and added booleans for css class control

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -21,7 +21,7 @@ import { FooterComponent } from './footer/footer.component';
     FooterComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule, FormsModule, ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Angular/src/app/login/login.component.ts
+++ Angular/src/app/login/login.component.ts
@@ -8,31 +8,47 @@ import { User } from '../user.module';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  @Input() 
+  @Input()
   usersDB: User[];
   loginFail: string;
   inputEmail: string;
   inputPassword: any;
   name: string;
-  public logIn() {  
+  validation: boolean;
+  failEmail: boolean;
+  failPsw: boolean;
+  public logIn() {      
    for(var user in this.usersDB){
-     if(this.usersDB[user].email == this.inputEmail && this.usersDB[user].password == this.inputPassword){
-       console.log("ok");
+      if(this.usersDB[user].email == this.inputEmail && this.usersDB[user].password == this.inputPassword){
+       this.validation = true;
+       this.failEmail = false;
+       this.failPsw = false;       
        sessionStorage.setItem("sessionStatus", "1");
        location.reload();
-     } else if (this.usersDB[user].email !== this.inputEmail && this.usersDB[user].password == this.inputPassword){  
-      this.loginFail = "Wrong Email";
+       break;
+    } else if (this.usersDB[user].email !== this.inputEmail && this.usersDB[user].password !== this.inputPassword){
+      this.failEmail = true;
+      this.failPsw = true; 
+      this.validation = false;        
+      this.loginFail = "Insert valid email and password!";         
       break;
-    } else if (this.usersDB[user].email == this.inputEmail && this.usersDB[user].password !== this.inputPassword){
-      this.loginFail = "Wrong Password";
+    } else if (this.usersDB[user].email !== this.inputEmail){
+      this.validation = false;  
+      this.loginFail = "Insert valid email!";
+      this.failEmail = true;
+      this.failPsw = false;    
       break;
-    }else if (this.usersDB[user].email !== this.inputEmail && this.usersDB[user].password !== this.inputPassword){
-      this.loginFail = "Wrong Fields";
+    } else if (this.usersDB[user].password !== this.inputPassword){
+      this.validation = false;        
+      this.loginFail = "Insert valid password!";
+      this.failPsw = true;
+      this.failEmail = false;
+      break;
     }
    }
   }
-
   constructor() {
-    sessionStorage.setItem("loggedUser", "0");   
+    sessionStorage.setItem("sessionStatus", "0");
+    this.validation = true;
   }
 }
