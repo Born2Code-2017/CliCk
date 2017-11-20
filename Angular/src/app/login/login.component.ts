23a7c156125265407363e25d71ba354a96c8b441
ado-ado <adolfinia@gmail.com>
2017-11-20 20:32:53 +0100

    Angular-> Login: Html, Css, Ts
    Html-Css-> Inserted glyphicon
    Ts-> Created 'forgot your password' method

--- Angular/src/app/login/login.component.ts
+++ Angular/src/app/login/login.component.ts
@@ -5,7 +5,8 @@ import { User } from '../user.module';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+
 })
 export class LoginComponent {
   @Input()
@@ -18,41 +19,65 @@ export class LoginComponent {
   failEmail: boolean;
   failPsw: boolean;
   sessionStatus: string;
-  public logIn() { 
-    for(var i=0; i<this.usersDB.length; i++){  
-      if(this.usersDB[i].email !== this.inputEmail && this.usersDB[i].password !== this.inputPassword) {
-      this.failEmail = true;
-      this.failPsw = true; 
-      this.validation = false;        
-      this.loginFail = "Insert valid email and password!";             
-    }     
-   }
-   for(var i=0; i<this.usersDB.length; i++){    
-      if(this.usersDB[i].email == this.inputEmail && this.usersDB[i].password == this.inputPassword){
-       this.validation = true;
-       this.failEmail = false;
-       this.failPsw = false; 
-       this.sessionStatus = "1";
-       sessionStorage.setItem("sessionStatus", this.sessionStatus);
-       sessionStorage.setItem("loggedUser", this.usersDB[i].id.toString());
-       location.reload();
-       break;
-    } else if (this.usersDB[i].email == this.inputEmail){
-      this.failPsw = true;
-      this.failEmail = false; 
-      this.validation = false;        
-      this.loginFail = "Insert valid password!";    
-    }else if (this.usersDB[i].password == this.inputPassword){   
-      this.failEmail = true;
-      this.failPsw = false; 
-      this.validation = false;
-      this.loginFail = "Insert valid email!";         
-    }  
-   } 
-  }
+  remember: string;
+  forgot: boolean;
+  button: string;
+
   constructor() {
     this.sessionStatus = "0";
     sessionStorage.setItem("sessionStatus", this.sessionStatus);
+    this.remember = 'Forgot your password?';
+  }
+
+  public logIn() {
+    for (var i = 0; i < this.usersDB.length; i++) {
+      if (this.usersDB[i].email !== this.inputEmail && this.usersDB[i].password !== this.inputPassword) {
+        this.failEmail = true;
+        this.failPsw = true;
+        this.validation = false;
+        this.loginFail = "Insert valid email and password!";
+      }
+    }
+    for (var i = 0; i < this.usersDB.length; i++) {
+      if (this.usersDB[i].email == this.inputEmail && this.usersDB[i].password == this.inputPassword) {
+        this.validation = true;
+        this.failEmail = false;
+        this.failPsw = false;
+        this.sessionStatus = "1";
+        sessionStorage.setItem("sessionStatus", this.sessionStatus);
+        sessionStorage.setItem("loggedUser", this.usersDB[i].id.toString());
+        location.reload();
+        break;
+      } else if (this.usersDB[i].email == this.inputEmail) {
+        this.failPsw = true;
+        this.failEmail = false;
+        this.validation = false;
+        this.loginFail = "Insert valid password!";
+      } else if (this.usersDB[i].password == this.inputPassword) {
+        this.failEmail = true;
+        this.failPsw = false;
+        this.validation = false;
+        this.loginFail = "Insert valid email!";
+      }
+    }
+  }
+
+  public forgotPsw() {
+    this.forgot = true;
     this.validation = true;
+  }
+  public send() {
+    for (var i = 0; i < this.usersDB.length; i++) {
+      if (this.usersDB[i].email == this.inputEmail) {
+        this.failEmail = false;
+        this.failPsw = false;
+        this.remember = "We" + "'" + "ve sent you an email ! ";
+        break;
+      } else if (this.usersDB[i].email !== this.inputEmail) {
+          this.failEmail = true;
+          this.failPsw = false;
+          this.remember = "Insert valid email!";
+        }
+    }
   }
 }
