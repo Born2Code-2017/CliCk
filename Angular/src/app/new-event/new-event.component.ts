0f62ee504da9ccf07fef7d40d3167400797664a4
Claudio <claudio_valente@hotmail.it>
2017-11-22 10:23:23 +0100

    Angular -> New-Event: Fix Placeholder

--- Angular/src/app/new-event/new-event.component.ts
+++ Angular/src/app/new-event/new-event.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Event } from '../event.module';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @Component({
   selector: 'app-new-event',
   templateUrl: './new-event.component.html',
   styleUrls: ['./new-event.component.css']
 })
-export class NewEventComponent {
+export class NewEventComponent implements OnInit {
+  @Input() eventsDB: Event[];
+  event: Event;
 
-  constructor() { }
+  constructor(private db: AngularFireDatabase) {
+    this.event = {
+      owner_id: undefined,
+      date: '',
+      time: '',
+      name: '',
+      city: '',
+      address: '',
+      going: undefined,
+      description: ''
+    };
+   }
+
+   ngOnInit() {
+    this.event.owner_id = parseInt(sessionStorage.getItem('loggedUser'), 10);
+    this.event.going = Math.floor((Math.random() * 1000) + 1);
+   }
+
+   createEvent(owner_id, date, time, name, city, address, going, description) {
+     this.db.database.ref('events/' + this.eventsDB.length).set(this.event);
+   }
 
 
 }
