200dd33ca6d50e9f5dd69ec0b093055666d32c50
Dario <loller790@gmail.com>
2017-11-23 16:04:42 +0100

    Angular->New-Event: Switch from AngularFire to HttpClient

--- Angular/src/app/new-event/new-event.component.ts
+++ Angular/src/app/new-event/new-event.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Event } from '../event.module';
-import { AngularFireDatabase } from 'angularfire2/database';
+
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-new-event',
@@ -9,9 +10,12 @@ import { AngularFireDatabase } from 'angularfire2/database';
 })
 export class NewEventComponent implements OnInit {
   @Input() eventsDB: Event[];
-  event: Event;
 
-  constructor(private db: AngularFireDatabase) {
+  event: Event;
+  
+  @Output() trashEventHide: EventEmitter<any> = new EventEmitter();
+
+  constructor(private http: HttpClient) {
     this.event = {
       owner_id: undefined,
       date: '',
@@ -25,16 +29,23 @@ export class NewEventComponent implements OnInit {
       trashedBy: ["-1"],
       checkedBy: ["-1"]
     };
-   }
+  }
 
-   ngOnInit() {
+  ngOnInit() {
+    sessionStorage.setItem("sessionStatus", "1");
     this.event.owner_id = parseInt(sessionStorage.getItem('loggedUser'), 10);
     this.event.going = Math.floor((Math.random() * 1000) + 1);
-   }
+    this.event.id = this.eventsDB.length;
+  }
 
-   createEvent(owner_id, date, time, name, city, address, going, description) {
-     this.db.database.ref('events/' + this.eventsDB.length).set(this.event);
-   }
+  createEvent(owner_id, date, time, name, city, address, going, description) {
+    this.eventsDB.push(this.event);
+    this.trashEventHide.emit(this.eventsDB);
+    let request = this.http.put("https://click-e25d0.firebaseio.com/click/events.json/", JSON.stringify(this.eventsDB));
+    request.subscribe(data => {
+      location.reload();
+    });
+  }
 
 
 }
