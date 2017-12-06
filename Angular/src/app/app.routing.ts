f5f7ada66ff06c9187373c8799d076d9f5acb637
Dario <loller790@gmail.com>
2017-12-06 12:55:08 +0100

    Angular: Fix routing on distribution builds

--- Angular/src/app/app.routing.ts
+++ Angular/src/app/app.routing.ts
@@ -27,7 +27,7 @@ const routes: Routes =
 
 @NgModule({
     imports: [
-        RouterModule.forRoot(routes)
+        RouterModule.forRoot(routes, { useHash: true })
     ],
     exports: [
         RouterModule
